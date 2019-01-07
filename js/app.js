b9c6ac5470e2562b0dc7addf87f999757293ffec
Jacob Mink <jacobmink@gmail.com>
2019-01-07 11:21:55 -0700

    Line piece made and moveable

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-console.log('test')
-
 const pieces = [
     'line',
     'square',
@@ -19,17 +17,58 @@ for (let i = 0; i < 21; i++){
     gameArr.push(arrayRow)
 }
 
-
-const lineFn = (index)=>{
-    for (let i = 3; i < 7; i++){
-        gameArr[index][i] = 1;
+class LinePiece {
+    constructor(){
+        this.occupiedSquares = [{'x': 5, 'y': 0},
+                                {'x': 6, 'y': 0},
+                                {'x': 7, 'y': 0},
+                                {'x': 8, 'y': 0}];
     }
-    console.log(gameArr);
+    render(){
+        $('.moving-piece').removeClass('moving-piece');
+        for (let i = 0; i < this.occupiedSquares.length; i++){
+            $(`.grid-square[x="${this.occupiedSquares[i]['x']}"][y="${this.occupiedSquares[i]['y']}"]`).addClass('moving-piece');
+        }
+    }
+    moveRight(){
+        if(this.occupiedSquares[3].x < 10){
+            this.occupiedSquares.forEach(function(element){
+                element['x']++;
+            })
+        }
+        this.render();
+    }
+    moveLeft(){
+        if(this.occupiedSquares[0].x > 0){
+            this.occupiedSquares.forEach(function(element){
+                element['x']--;
+            })
+        }
+        this.render();
+    }
+    
+}
+
+const linePiece = new LinePiece();
+
+
+
+const lineFn = (arr)=>{
+    for(let i = 0; i < 4; i++){
+        arr[game.arrIndex][i] = 1;
+        $(`.grid-square[x="${i}"][y="${game.arrIndex - 1}"]`).removeClass('moving-piece').addClass('background-piece');
+        $(`.grid-square[x="${i}"][y="${game.arrIndex}"]`).addClass('moving-piece');
+       
+
+    }
+    game.arrIndex++;
+    console.log(arr);
 }
 
 const game = {
     time: 0,
-    pieceList: pieces
+    pieceList: pieces,
+    arrIndex: 0
 }
 
 
@@ -41,27 +80,26 @@ const makeStats = ()=>{
 }
 
 const makeGrid = ()=>{
-    for (let i = 20; i > -1; i--){
-        $('.game-board').attr('style','border: 1px solid black;');
-        const $gridRow = $('<div/>').addClass('row grid-row').attr('row', i);
-        $('.game-board').append($gridRow);
+    for (let y = 0; y < 21; y++){
+        const $gridRow = $('<div/>').addClass('grid-row');
+        
         for (let x = 0; x < 11; x++){
-            const $gridSquare = $('<div/>').addClass('col border grid-square').text(`${x},${i}`);
-            $gridSquare.attr('coord', {x,i});
+            const $gridSquare = $('<div/>').addClass('grid-square').attr('x',x).attr('y',y);
+            // $gridSquare.attr('coord', {x,i});
             $($gridRow).append($gridSquare);
         }
+        $('.game-board').append($gridRow);
     }
 }
-let gameArrIndex = 0;
 const fallingPieces = ()=>{
     game.time++;
-    gameArrIndex++;
-    lineFn(gameArrIndex);
-    
-
-    // const randNum = Math.floor(Math.random()*(gameArr.length-1));
-    // $('.grid-square').attr('style','background-color: black;')
-    
+    if(game.arrIndex < gameArr.length){
+        linePiece.render();
+        linePiece.occupiedSquares.forEach(function(element){
+            element['y']++;
+        })
+        game.arrIndex++;
+    }
 }
 
 let timePass;
@@ -70,10 +108,15 @@ $('#start-button').click((e)=>{
     $(e.target).hide();
     makeGrid();
     makeStats();
-    timePass = setInterval(fallingPieces, 1000);
+    timePass = setInterval(()=>{
+        fallingPieces()
+    }, 1000);
 })
 
-// $('body').on('keydown', function(e){
-
-//     // if(e.which == 39)
-// })
+$('body').on('keydown', function(e){
+    if(e.which == 39){
+        linePiece.moveRight();
+    }else if(e.which == 37){
+        linePiece.moveLeft();
+    }
+})
